1325d98668fac0a98773f51d5b401a35419be9b4
DESKTOP-JO3QE3M\82108 <leejin914418@gmail.com>
2023-10-01 21:45:46 +0900

    5주차

--- 4w/main.js
+++ 4w/main.js
@@ -1,161 +1,158 @@
+"use strict";
 const data = [
-  {
-    name: "Bulbasaur",
-    height: "7",
-    weight: "69",
-    types: ["grass", "poison"],
-    "base-Experience": "64",
-    abilities: ["overgrow", "chlorophyll"],
-    hp: "45",
-    attack: "49",
-    defense: "49",
-    "special-attack": "65",
-    "special-defense": "65",
-    speed: "45",
-  },
-  {
-    name: "Ivysaur",
-    height: "10",
-    weight: "130",
-    types: ["grass", "poison"],
-    "base-Experience": "142",
-    abilities: ["overgrow", "chlorophyll"],
-    hp: "60",
-    attack: "62",
-    defense: "63",
-    "special-attack": "80",
-    "special-defense": "80",
-    speed: "60",
-  },
-  {
-    name: "Venusaur",
-    height: "20",
-    weight: "1000",
-    types: ["grass", "poison"],
-    "base-Experience": "236",
-    abilities: ["overgrow", "chlorophyll"],
-    hp: "80",
-    attack: "82",
-    defense: "83",
-    "special-attack": "100",
-    "special-defense": "100",
-    speed: "80",
-  },
-  {
-    name: "Charmander",
-    height: "6",
-    weight: "85",
-    types: ["fire"],
-    "base-Experience": "62",
-    abilities: ["blaze", "solar-power"],
-    hp: "39",
-    attack: "52",
-    defense: "43",
-    "special-attack": "60",
-    "special-defense": "50",
-    speed: "65",
-  },
-  {
-    name: "Charmeleon",
-    height: "11",
-    weight: "190",
-    types: ["fire"],
-    "base-Experience": "142",
-    abilities: ["blaze", "solar-power"],
-    hp: "58",
-    attack: "64",
-    defense: "58",
-    "special-attack": "80",
-    "special-defense": "65",
-    speed: "80",
-  },
-  {
-    name: "Charizard",
-    height: "17",
-    weight: "905",
-    types: ["fire", "flying"],
-    "base-Experience": "240",
-    abilities: ["blaze", "solar-power"],
-    hp: "78",
-    attack: "84",
-    defense: "78",
-    "special-attack": "109",
-    "special-defense": "85",
-    speed: "100",
-  },
-  {
-    name: "Squirtle",
-    height: "5",
-    weight: "90",
-    types: ["water"],
-    "base-Experience": "63",
-    abilities: ["torrent", "rain-dish"],
-    hp: "44",
-    attack: "48",
-    defense: "65",
-    "special-attack": "50",
-    "special-defense": "64",
-    speed: "43",
-  },
-  {
-    name: "Wartortle",
-    height: "10",
-    weight: "225",
-    types: ["water"],
-    "base-Experience": "142",
-    abilities: ["torrent", "rain-dish"],
-    hp: "59",
-    attack: "63",
-    defense: "80",
-    "special-attack": "65",
-    "special-defense": "80",
-    speed: "58",
-  },
-  {
-    name: "Blastoise",
-    height: "16",
-    weight: "855",
-    types: ["water"],
-    "base-Experience": "239",
-    abilities: ["torrent", "rain-dish"],
-    hp: "79",
-    attack: "83",
-    defense: "100",
-    "special-attack": "85",
-    "special-defense": "105",
-    speed: "78",
-  },
-  {
-    name: "Caterpie",
-    height: "3",
-    weight: "29",
-    types: ["bug"],
-    "base-Experience": "39",
-    abilities: ["shield-dust", "run-away"],
-    hp: "45",
-    attack: "30",
-    defense: "35",
-    "special-attack": "20",
-    "special-defense": "20",
-    speed: "45",
-  },
+    {
+        name: "Bulbasaur",
+        height: "7",
+        weight: "69",
+        types: ["grass", "poison"],
+        "base-Experience": "64",
+        abilities: ["overgrow", "chlorophyll"],
+        hp: "45",
+        attack: "49",
+        defense: "49",
+        "special-attack": "65",
+        "special-defense": "65",
+        speed: "45",
+    },
+    {
+        name: "Ivysaur",
+        height: "10",
+        weight: "130",
+        types: ["grass", "poison"],
+        "base-Experience": "142",
+        abilities: ["overgrow", "chlorophyll"],
+        hp: "60",
+        attack: "62",
+        defense: "63",
+        "special-attack": "80",
+        "special-defense": "80",
+        speed: "60",
+    },
+    {
+        name: "Venusaur",
+        height: "20",
+        weight: "1000",
+        types: ["grass", "poison"],
+        "base-Experience": "236",
+        abilities: ["overgrow", "chlorophyll"],
+        hp: "80",
+        attack: "82",
+        defense: "83",
+        "special-attack": "100",
+        "special-defense": "100",
+        speed: "80",
+    },
+    {
+        name: "Charmander",
+        height: "6",
+        weight: "85",
+        types: ["fire"],
+        "base-Experience": "62",
+        abilities: ["blaze", "solar-power"],
+        hp: "39",
+        attack: "52",
+        defense: "43",
+        "special-attack": "60",
+        "special-defense": "50",
+        speed: "65",
+    },
+    {
+        name: "Charmeleon",
+        height: "11",
+        weight: "190",
+        types: ["fire"],
+        "base-Experience": "142",
+        abilities: ["blaze", "solar-power"],
+        hp: "58",
+        attack: "64",
+        defense: "58",
+        "special-attack": "80",
+        "special-defense": "65",
+        speed: "80",
+    },
+    {
+        name: "Charizard",
+        height: "17",
+        weight: "905",
+        types: ["fire", "flying"],
+        "base-Experience": "240",
+        abilities: ["blaze", "solar-power"],
+        hp: "78",
+        attack: "84",
+        defense: "78",
+        "special-attack": "109",
+        "special-defense": "85",
+        speed: "100",
+    },
+    {
+        name: "Squirtle",
+        height: "5",
+        weight: "90",
+        types: ["water"],
+        "base-Experience": "63",
+        abilities: ["torrent", "rain-dish"],
+        hp: "44",
+        attack: "48",
+        defense: "65",
+        "special-attack": "50",
+        "special-defense": "64",
+        speed: "43",
+    },
+    {
+        name: "Wartortle",
+        height: "10",
+        weight: "225",
+        types: ["water"],
+        "base-Experience": "142",
+        abilities: ["torrent", "rain-dish"],
+        hp: "59",
+        attack: "63",
+        defense: "80",
+        "special-attack": "65",
+        "special-defense": "80",
+        speed: "58",
+    },
+    {
+        name: "Blastoise",
+        height: "16",
+        weight: "855",
+        types: ["water"],
+        "base-Experience": "239",
+        abilities: ["torrent", "rain-dish"],
+        hp: "79",
+        attack: "83",
+        defense: "100",
+        "special-attack": "85",
+        "special-defense": "105",
+        speed: "78",
+    },
+    {
+        name: "Caterpie",
+        height: "3",
+        weight: "29",
+        types: ["bug"],
+        "base-Experience": "39",
+        abilities: ["shield-dust", "run-away"],
+        hp: "45",
+        attack: "30",
+        defense: "35",
+        "special-attack": "20",
+        "special-defense": "20",
+        speed: "45",
+    },
 ];
-
 const pokemonMap = new Map(); //맵 만들기
-data.forEach((pokemon) => pokemonMap.set(pokemon.name, { ...pokemon })); //포켓몬 맵 만들기
-
-document.addEventListener("DOMContentLoaded", () => {
-  //에러떠서 추가
-  const container = document.querySelector(".container"); //container 요소 찾아서 가져오기
-  let index = 1;
-  pokemonMap.forEach((value, key, map) => {
+data.forEach((pokemon) => pokemonMap.set(pokemon.name, Object.assign({}, pokemon))); //포켓몬 맵 만들기
+const container = document.querySelector(".container"); //container 요소 찾아서 가져오기
+let index = 1;
+pokemonMap.forEach((value, key, map) => {
     let currentindex = index;
     let item = document.createElement("div"); //item 블럭 만들기
     item.className = "item";
     item.onclick = () => {
-      localStorage.setItem("pokemon", JSON.stringify(data[currentindex - 1])); //currentindex 대신에 객체 전달하도록 바꿈
-      localStorage.setItem("index", currentindex); //img src 링크 위해 index도 전달
-      window.location.href = "./pokemon/"; //a태그 대신에 onclick 이벤트
+        localStorage.setItem("pokemon", JSON.stringify(data[currentindex - 1])); //currentindex 대신에 객체 전달하도록 바꿈
+        localStorage.setItem("index", currentindex.toString()); //img src 링크 위해 index도 전달
+        window.location.href = "./pokemon/"; //a태그 대신에 onclick 이벤트
     };
     let imgWrapper = document.createElement("div");
     let pokeimg = document.createElement("img");
@@ -166,16 +163,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let height = document.createElement("p");
     let weight = document.createElement("p");
     let types = document.createElement("p");
-
     name.innerHTML = `${key}`;
     height.innerHTML = `height: ${value.height}dm`;
     weight.innerHTML = `weight: ${value.weight}hg`;
     types.innerHTML = `types: ${value.types}`;
-
     item.append(imgWrapper, infowrapper);
     imgWrapper.append(pokeimg);
     infowrapper.append(name, height, weight, types);
     container.append(item);
     index++;
-  });
 });
--- 4w/pokemon/main.ts
+++ 4w/pokemon/main.ts
@@ -1,8 +1,26 @@
-"use strict";
+interface PokemonData {
+  name: string;
+  height: string;
+  weight: string;
+  types: string[];
+  "base-Experience": string;
+  abilities: string[];
+  hp: string;
+  attack: string;
+  defense: string;
+  "special-attack": string;
+  "special-defense": string;
+  speed: string;
+}
+
 let storedpokemon = localStorage.getItem("pokemon"); //Json 문자열 형태로 저장됨
-let pokemon = storedpokemon ? JSON.parse(storedpokemon) : null; //객체로 변환
+let pokemon: PokemonData | null = storedpokemon
+  ? JSON.parse(storedpokemon)
+  : null; //객체로 변환
 console.log(pokemon); //객체
+
 let storedindex = localStorage.getItem("index");
+
 document.addEventListener("DOMContentLoaded", () => {
   const imgWrapper = document.querySelector(".img-wrapper"); //imgwrapper 요소 찾아서 가져오기
   const main = document.querySelector("main");
@@ -13,12 +31,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let tableWrapper = document.createElement("div");
   tableWrapper.className = "table-wrapper";
   let table = document.createElement("table");
-  let map = new Map();
+
+  let map = new Map<string, string>();
   if (pokemon) {
     Object.entries(pokemon).forEach(([key, value]) => {
       map.set(key, value);
     });
   } //객체를 맵으로 바꿔주기
+
   map.forEach((value, key, map) => {
     const tr = document.createElement("tr");
     const td1 = document.createElement("td");
@@ -28,10 +48,12 @@ document.addEventListener("DOMContentLoaded", () => {
     table.append(tr);
     tr.append(td1, td2);
   });
+
   //자식요소로 추가해주기
   tableWrapper.append(table);
   imgWrapper ? imgWrapper.append(img) : null;
   main ? main.append(name, tableWrapper) : null;
+
   //title 변경까지
   pokemon ? (document.title = `${pokemon.name}`) : null;
 });
